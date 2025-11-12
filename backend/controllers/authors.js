bd0b7b660552fa76049649327e69ae31b006b888
yewinthlaing04 <yewinthlaing04@gmail.com>
2025-11-12 16:15:14 +0100

    architect md

--- backend/controllers/authors.js
+++ backend/controllers/authors.js
@@ -32,4 +32,105 @@ const createAuthor = async (req, res) => {
   }
 };
 
-export { createAuthor };
+const listAuthor = async (req, res) => {
+  try {
+    logger.info("List All Author endpoint ... ");
+
+    const authorlist = await Author.find();
+
+    if (authorlist.length === 0) {
+      logger.warn("There is no author list in db ");
+      return res.status(200).json({
+        success: true,
+        message: "There is no author in db",
+        list: [],
+      });
+    }
+
+    return res.status(201).json({
+      success: true,
+      list: authorlist,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const updateAuthor = async (req, res) => {
+  try {
+    const id = req.params;
+    const updateData = req.body;
+
+    logger.info("Updating Author Endpoint... ");
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid author ID",
+      });
+    }
+
+    const author = await Author.findByIdAndUpdate(
+      id,
+      { $set: updateData },
+      { new: true, runValidators: true }
+    );
+
+    if (!author) {
+      logger.warn("Author doesn't exist");
+
+      return res.status(404).json({
+        success: false,
+        message: "Author not found ",
+      });
+    }
+
+    logger.info("Updated Author successfully... ");
+
+    return res.status(200).json({
+      success: true,
+      message: "Update Author successfully... ",
+      author: author,
+    });
+  } catch (error) {
+    return res.status(500).status({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const deleteAuthor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    logger.info("Deleting Author by id endpoint");
+
+    // find the author with id
+    const author = await Author.findByIdAndDelete(id);
+
+    if (!author) {
+      logger.warn("Author doesn't exist with id");
+      return res.status(404).json({
+        success: false,
+        message: "Author not found",
+      });
+    }
+
+    logger.info("Author is deleted successfully");
+
+    return res.status(200).json({
+      success: true,
+      message: "Author is deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { createAuthor, listAuthor, deleteAuthor, updateAuthor };
